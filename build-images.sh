27fe67f508c42058b2b67b79f7b11e060bc71788
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-02-07 15:47:08 +0100

    Use bitnami mariadb container

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/bitnami/moodle:4.1" \
+    --label="org.nethserver.images=docker.io/bitnami/mariadb:latest docker.io/bitnami/moodle:4.1" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
