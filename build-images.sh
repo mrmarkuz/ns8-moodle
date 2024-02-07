639df04c09fbf57bfd0ad70081f28434b9beefd3
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-02-07 14:25:27 +0100

    Fix image registry

--- build-images.sh
+++ build-images.sh
@@ -11,7 +11,7 @@ set -e
 # Prepare variables for later use
 images=()
 # The image will be pushed to GitHub container registry
-repobase="${REPOBASE:-ghcr.io/mrmarkuz}"
+repobase="${REPOBASE:-ghcr.io/nethserver}"
 # Configure the image name
 reponame="moodle"
 
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/mariadb:10.11.5 bitnami/moodle:4.1" \
+    --label="org.nethserver.images=docker.io/mariadb:10.11.5 docker.io/bitnami/moodle:4.1" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
