9b5ed6921dd6435bc933102aa77fb2e41bb694c2
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-02-08 17:10:56 +0100

    Fix image tags

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/moodlehq/moodle-db-mssql:latest docker.io/moodlehq/moodle-php-apache:latest" \
+    --label="org.nethserver.images=docker.io/moodlehq/moodle-db-mssql docker.io/moodlehq/moodle-php-apache" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
