441bd87a1c0216f7dbd609e64fc3bb12c85a9354
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-02-08 16:54:43 +0100

    Use moodlehq docker containers

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/moodlehq/moodleapp:latest" \
+    --label="org.nethserver.imagesdocker.io/moodlehq/moodle-db-mssql:latest docker.io/moodlehq/moodle-php-apache:latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
