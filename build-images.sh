777c3274f318890ec3fafc5cea920ed2bc440b79
Markus Neuberger <31746411+mrmarkuz@users.noreply.github.com>
2024-05-26 18:43:24 +0200

    Use 4.4.0 instead latest as version tag and fix backup/restore db

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/bitnami/mariadb:latest docker.io/bitnami/moodle:latest" \
+    --label="org.nethserver.images=docker.io/bitnami/mariadb:11.2.4 docker.io/bitnami/moodle:4.4.0" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
