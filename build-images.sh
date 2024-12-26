76c009ba1219bcb1449414f37bb810021ef8138a
mrmarkuz <mrmarkuz666@gmail.com>
2024-12-26 18:23:05 +0100

    Raise versions and add renovate config

--- build-images.sh
+++ build-images.sh
@@ -45,7 +45,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/bitnami/mariadb:11.2.4 docker.io/bitnami/moodle:4.4.0" \
+    --label="org.nethserver.images=docker.io/bitnami/mariadb:11.5.2 docker.io/bitnami/moodle:4.5.1" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
